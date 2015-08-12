2456c36007cf27164bc41f1f1e2d5783b2467b0c
Mike Chastain & Christopher Calascione <student@epicodus.com>
2015-08-12 16:42:32 -0700

    creates mockup functionality with a user interface

--- app/app.php
+++ app/app.php
@@ -23,17 +23,31 @@
 
 
     $app->get("/delete_all_players", function() use ($app) {
+        $_SESSION['list_of_current_players'] = array();
      Player::deleteAll();
      return $app['twig']->render('form.html.twig', array('players' => Player::getAll()));
  });
 
 
     $app->post("/create_player", function() use ($app) {
+        $_SESSION['list_of_current_players'] = array();
         $player = new Player($_POST['name'], $_POST['age'], $_POST['image'], $_POST['email']);
         $player->save();
 //var_dump(Player::getAll()[0]);
         return $app['twig']->render('form.html.twig', array('players' => Player::getAll()));
     });
+
+    $app->get("/game", function() use ($app) {
+
+        return $app['twig']->render('game.html.twig');
+    });
+
+    $app->post("/results", function() use ($app) {
+        $rps = new RPS();
+        $result = $rps->returnWinner($_POST['player1'], $_POST['player2']);
+        return $app['twig']->render('results.html.twig', array('result' => $result));
+    });
+
 
     return $app;
 
