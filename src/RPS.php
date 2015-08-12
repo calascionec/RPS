9ef8a7f7fbeb74013b42d3fa94cde1dc6a4c65d5
Mike Chastain & Christopher Calascione <student@epicodus.com>
2015-08-12 11:13:06 -0700

    Create RPS class to pass all tests

--- src/RPS.php
+++ src/RPS.php
@@ -3,11 +3,43 @@
     class RPS
     {
 
-        function returnWinner($player1,$player2)
-                {
 
-                    return "Fail!!!";
+
+        function returnWinner($player1,$player2)
+            {
+                if ($player1 === "rock") {
+                    if ($player2 === "paper") {
+                        return "Player 2";
+                    } elseif ($player2 === "scissors"){
+                        return "Player 1";
+                    } else {
+                        return "Tie";
+                    }
                 }
+
+                if ($player1 === "paper") {
+                    if ($player2 === "paper") {
+                        return "Tie";
+                    } elseif ($player2 === "scissors"){
+                        return "Player 2";
+                    } else {
+                        return "Player 1";
+                    }
+                }
+
+                if ($player1 === "scissors") {
+                    if ($player2 === "paper") {
+                        return "Player 1";
+                    } elseif ($player2 === "scissors"){
+                        return "Tie";
+                    } else {
+                        return "Player 2";
+                    }
+                }
+
+            }
+
+
 
     }
 
