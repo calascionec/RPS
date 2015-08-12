6ca989f8cecec0d80559141191f41965de4f81dc
Mike Chastain & Christopher Calascione <student@epicodus.com>
2015-08-12 14:44:52 -0700

    creates basic game functionality

--- app/app.php
+++ app/app.php
@@ -1,4 +1,5 @@
 <?php
+
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/RPS.php";
 
@@ -7,14 +8,16 @@
     $app->register(new Silex\Provider\TwigServiceProvider(),array('twig.path' => __DIR__.'/../views'));
 
     $app->get("/", function() use ($app){
-            return $app['twig']->render('form.html.twig');
+            return $app['twig']->render('game.html.twig');
 
+    });
+
+    $app->post("/results", function() use ($app) {
+        $rps = new RPS();
+        $result = $rps->returnWinner($_POST['player1'], $_POST['player2']);
+        return $app['twig']->render('results.html.twig', array('result' => $result));
     });
 
     return $app;
 
-
-
-
-
-?>
+ ?>
---
+++ app/app2.php
@@ -0,0 +1,44 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RPS.php";
+    require_once __DIR__."/../src/Player.php";
+
+    session_start();
+
+    if (empty($_SESSION['list_of_players'])) {
+    $_SESSION['list_of_players'] = array();
+    }
+    if (empty($_SESSION['list_of_current_players'])) {
+    $_SESSION['list_of_current_players'] = array();
+    }
+
+    $app = new Silex\Application();
+    $app['debug']  = true;
+    $app->register(new Silex\Provider\TwigServiceProvider(),array('twig.path' => __DIR__.'/../views'));
+
+    $app->get("/", function() use ($app){
+            return $app['twig']->render('form.html.twig', array('players' => Player::getAll()));
+
+    });
+
+
+    $app->get("/delete_all_players", function() use ($app) {
+     Player::deleteAll();
+     return $app['twig']->render('form.html.twig', array('players' => Player::getAll()));
+ });
+
+
+    $app->post("/create_player", function() use ($app) {
+        $player = new Player($_POST['name'], $_POST['age'], $_POST['image'], $_POST['email']);
+        $player->save();
+        var_dump(Player::getAll()[0]);
+        return $app['twig']->render('form.html.twig', array('players' => Player::getAll()));
+    });
+
+    return $app;
+
+
+
+
+
+?>
